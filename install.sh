d12e27db63dbfefbc11b0b1cb12d12615e7bbcbf
JackOXI <jack@oxide.host>
2021-06-09 13:12:12 +0100

    Made more important changes

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
+if ! [ -x "$(command -v curl)" ]; then
+    apt -y update
+    apt install -y curl
+fi
 cd /var/www/pterodactyl/database/Seeders
 rm NestSeeder.php
-wget https://github.com/JackW6809/pterodactyl-parker-eggs-install-script/releases/latest/download/data.zip
-unzip -n master.zip
+curl -Lo data.zip https://github.com/JackW6809/pterodactyl-parker-eggs-install-script/releases/download/v1.0-beta/data.zip
+unzip -n data.zip
 rm data.zip
 cd ../../
 php artisan migrate --seed --force
