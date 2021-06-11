1bd447db2d24fa78cbe1f0ca5cce7ee2d04a8be9
JackOXI <jack@oxide.host>
2021-06-12 00:39:51 +0100

    No more

--- installer/installer.sh
+++ installer/installer.sh
@@ -1,7 +1,11 @@
+port=$1
 if ! [ -x "$(command -v curl)" ]; then
     apt -y update
     apt install -y curl
 fi
+php artisan p:environment:setup -n --author=test@test.com --url=https://panel.test.com --timezone=Europe/London --cache=file --session=file --queue=sync
+php artisan p:environment:database --host=127.0.0.1 --port=$port --database=panel_test --username=root --password=""
+php artisan migrate --seed --force
 cd database/Seeders
 mkdir temp
 mv NestSeeder.php EggSeeder.php temp/
